c321d6b3b5374eeebcd9ecbf3e1f0a5cf745cfe7
pave1ivanov <ivanovcopyright@gmail.com>
2022-04-02 16:17:12 +0500

    added login page and login page locators

---
+++ pages/login_page.py
@@ -0,0 +1,20 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert LoginPageLocators.LOGIN_PAGE_URL in self.browser.current_url, "Login page URL is not correct"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert True
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert True
