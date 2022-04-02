0d9b0d5532e6ace211531a9f445afddb20329ba5
pave1ivanov <ivanovcopyright@gmail.com>
2022-04-02 18:12:14 +0500

    added product page testing

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,10 @@ class LoginPageLocators():
     URL = "/accounts/login/"
     LOGIN_FROM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    
+class ProductPageLocators():
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main > h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main > .price_color")
+    SUCCESS_MESSAGE_PRODUCT_NAME = (By.CSS_SELECTOR, "#messages .alert:first-child > .alertinner > strong")
+    INFO_MESSAGE_PRODUCT_PRICE = (By.CSS_SELECTOR, "#messages .alert-info > .alertinner strong")
---
+++ pages/product_page.py
@@ -0,0 +1,18 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+class ProductPage(BasePage):
+
+    def add_product_to_basket(self):
+        add_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
+        add_button.click()
+
+    def should_be_correct_product_in_success_message(self):
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        success_message_product_name = self.browser.find_element(*ProductPageLocators.SUCCESS_MESSAGE_PRODUCT_NAME).text
+        assert product_name in success_message_product_name, f"Product name '{product_name}'in success message is not correct - '{success_message_product_name}'"
+
+    def should_be_correct_price_in_info_message(self):
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        info_message_product_price = self.browser.find_element(*ProductPageLocators.INFO_MESSAGE_PRODUCT_PRICE).text
+        assert product_price in info_message_product_price, f"Product price '{product_price}'in info message is not correct - '{info_message_product_price}'"
