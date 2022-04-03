b28e9914c8e8a6686ae79cd00763bb9e548343ee
pave1ivanov <ivanovcopyright@gmail.com>
2022-04-03 09:44:13 +0500

    parametrized link in  test_guest_can_add_product_to_basket

--- pages/product_page.py
+++ pages/product_page.py
@@ -10,9 +10,9 @@ class ProductPage(BasePage):
     def should_be_correct_product_in_success_message(self):
         product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
         success_message_product_name = self.browser.find_element(*ProductPageLocators.SUCCESS_MESSAGE_PRODUCT_NAME).text
-        assert product_name in success_message_product_name, f"Product name '{product_name}'in success message is not correct - '{success_message_product_name}'"
+        assert product_name == success_message_product_name, f"Product name '{product_name}'in success message is not correct - '{success_message_product_name}'"
 
     def should_be_correct_price_in_info_message(self):
         product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
         info_message_product_price = self.browser.find_element(*ProductPageLocators.INFO_MESSAGE_PRODUCT_PRICE).text
-        assert product_price in info_message_product_price, f"Product price '{product_price}'in info message is not correct - '{info_message_product_price}'"
+        assert product_price == info_message_product_price, f"Product price '{product_price}'in info message is not correct - '{info_message_product_price}'"
